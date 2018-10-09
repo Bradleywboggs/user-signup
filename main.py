0d8f1ec54bf6d38644095f46399ef8efd44c162c
Bradley Boggs <bradleywboggs@gmail.com>
2018-10-09 18:53:30 -0500

    Added email validation and recovered lost changes from previous commit

--- main.py
+++ main.py
@@ -17,60 +17,84 @@ def validate_signup():
     username_error = ''
     password_error = ''
     email_error = ''
+    verifypw_error = ''
 
 #validate username
     if username == '':
-        username_error = "This field is required"
+        username_error = "This field is required."
         username = ''
     elif len(username) < 3:
-        username_error = "Username is not valid"
+        username_error = "Username is not valid."
     elif len(username) > 20:
-        username_error = "Username is not valid"
+        username_error = "Username is not valid."
         username = ''
     elif username[0] == " ":
-        username_error = "Username is not valid"
+        username_error = "Username is not valid."
         username = ''
     else:
         username = username
         username_error = ''
     
-# validate Password
+# validate password
     if password == '':
-        password_error = "This field is required"
+        password_error = "This field is required."
         password = ''
-        verifypw = ''
+        verifypw = ''      
     elif len(password) < 3:
-        username = username
-        password_error = "Password is not valid"
+        password_error = "Password is not valid."
         password = ''
         verifypw = ''
     elif len(password) > 20:
-        username = username
-        password_error = "Password is not valid"
+        password_error = "Password is not valid."
         password = ''
-        verifypw = ''
+        verifypw = ''     
+# verify both passwords match
     elif password != verifypw:
-        username = username
-        password_error = "Passwords do not match"
+        verifypw_error = "Passwords do not match."
         password = ''
         verifypw = ''
     else:
         username = username
         password = password
         verifypw = verifypw
+ 
+        
+    
+# validate email
+    if email == '':
+        email_error = ''
+        email = email
+    elif " " in email:
+        email_error = "Email is not valid."
+        email = ''
+    elif "." not in email:
+        email_error = "Email is not valid."
+        email = ''
+    elif "@" not in email:
+        email_error = "Email is not valid."
+        email = ''
+    elif len(email) < 3:
+        email_error = "Email is not valid."
+        email = ''
+    elif len(email) > 24:
+        email_error = "Email is not valid."
+        email = ''
+    else:
+        email_error = ''
+        email = email
 
-    if not username_error and not password_error:
+    if not username_error and not password_error and not email_error and not verifypw_error:
         return redirect(f"/welcome?username={username}")
     #render the page again with error descriptions
     else:
         return render_template("signup_form.html", username_error=username_error, 
-        password_error=password_error, email_error=email_error, username=username,
+        password_error=password_error, email_error=email_error, verifypw_error=verifypw_error, username=username,
         password='', verifypw='', email=email)
 
 
 @app.route("/welcome")
 def welcome():
     username = request.args.get("username")
-    return f"<h2>Welcome, {username}!<h2>"
+    return render_template("welcome.html", username=username)
 
 app.run()
